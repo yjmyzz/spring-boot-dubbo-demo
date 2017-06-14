6a0f1e8dc18f488c805643ac68b03a18fec50150
yangjunming <1q2w3e4r->
2017-06-14 21:37:41 +0800

    add mybatis/druid 集成

---
+++ service-provider/src/main/java/com/cnblogs/yjmyzz/dao/mapper/CityMapper.java
@@ -0,0 +1,7 @@
+package com.cnblogs.yjmyzz.dao.mapper;
+
+import com.cnblogs.yjmyzz.dao.model.City;
+import com.cnblogs.yjmyzz.util.MyMapper;
+
+public interface CityMapper extends MyMapper<City> {
+}
---
+++ service-provider/src/main/java/com/cnblogs/yjmyzz/dao/model/City.java
@@ -0,0 +1,54 @@
+package com.cnblogs.yjmyzz.dao.model;
+
+import javax.persistence.*;
+
+public class City {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String state;
+
+    /**
+     * @return id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return state
+     */
+    public String getState() {
+        return state;
+    }
+
+    /**
+     * @param state
+     */
+    public void setState(String state) {
+        this.state = state;
+    }
+}
